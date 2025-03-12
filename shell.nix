11815c99663c0788a9192250513a907738280550
Drum3X <86149093+Drum3X@users.noreply.github.com>
2025-03-12 20:53:56 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -15,7 +15,7 @@ pkgs.mkShell {
     gdk-pixbuf
     gnome.gnome-bluetooth
     cinnamon.cinnamon-desktop
-    (python3.withPackages (
+    (python313.withPackages (
       ps: with ps; [
         setuptools
         wheel
